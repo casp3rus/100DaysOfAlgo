c3c9e2b5df5f94d4093860a435d6704ae779bdfb
Adrian Popan <adrian.popan@gmail.com>
2020-12-16 22:25:18 -0600

    Day 006 - ProductSum

--- 006ProductSum/productSum.js
+++ 006ProductSum/productSum.js
@@ -1,2 +1,14 @@
 // Solution 1
-// 
+// O(n) time / O(d) - space "d - depth of the nested array"
+
+const productSum = (array, multiplier = 1) => {
+    let sum = 0
+    for (const element of array) {
+        if (Array.isArray(element)) {
+            sum += productSum(element, multiplier + 1)
+        } else {
+            sum += element
+        }
+    }
+    return sum * multiplier
+}

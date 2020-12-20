35f3bbaa0039846349ac4a99c083828f5020dd42
Adrian Popan <adrian.popan@gmail.com>
2020-12-20 13:57:05 -0600

    Day 010 - Insertion Sort

---
+++ 010InsertionSort/insertionSort.py
@@ -0,0 +1,13 @@
+# Solution 1
+# O(n^2) time / O(1) space 
+
+def insertionSort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j - 1]:
+            swap(j, j - 1, array)
+            j -= 1
+    return array
+
+def swap(i, j, array):
+    arra[i], array[j] = array[j], array[i]

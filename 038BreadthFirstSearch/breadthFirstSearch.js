066ac42bf8a02ea312b67a009b61ebc1593a25ca
Adrian Popan <adrian.popan@gmail.com>
2021-01-17 20:02:20 -0600

    Day 038 - Breadth First Search

---
+++ 038BreadthFirstSearch/breadthFirstSearch.js
@@ -0,0 +1,27 @@
+// Solution
+// O(n) time / O(n) space
+// n - number of nodes
+
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  breadthFirstSearch(array) {
+    const queue = [this];
+    while (queue.length > 0) {
+      const current = queue.shift();
+      array.push(current.name);
+      for (const child of current.children) {
+        queue.push(child);
+      }
+    }
+    return array;
+  }
+}

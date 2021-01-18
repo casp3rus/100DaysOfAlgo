a11d9dec363f6daa6d8198b72ae302ff320ad2a3
Adrian Popan <adrian.popan@gmail.com>
2021-01-17 21:21:41 -0600

    Day 038 - Breadth First Search

---
+++ 038BreadthFirstSearch/breadthFirstSearch.rb
@@ -0,0 +1,29 @@
+# Solution
+# O(n) time / O(n) space
+# n - number of nodes
+
+class Node
+
+    attr_accessor :name, :children
+
+    def initialize(name)
+        @name = name
+        @children = []
+    end
+
+    def add_child(name)
+        @children << Node.new(name)
+    end
+
+    def breadth_first_search(array)
+        queue = [self]
+        while !queue.empty?
+            current = queue.shift
+            array << current.name
+            current.children.each do |child|
+                queue << child
+            end
+        end
+        return array
+    end
+end

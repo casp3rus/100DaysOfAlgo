6d636ec0798503d0611d2cd77a808f2a04715bc5
Adrian Popan <adrian.popan@gmail.com>
2021-01-23 17:36:18 -0600

    Day 044 - Remove Kth Node From List

---
+++ 044RemoveKthNodeFromEnd/removeKthNodeFromEnd.js
@@ -0,0 +1,28 @@
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// O(n) time / O(1) space
+
+const removeKthNodeFromEnd = (head, k) => {
+  let counter = 1;
+  let first = head;
+  let second = head;
+  while (counter <= k) {
+    second = second.next;
+    counter++;
+  }
+  if (second === null) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+    return;
+  }
+  while (second.next !== null) {
+    second = second.next;
+    first = first.next;
+  }
+  first.next = first.next.next;
+};

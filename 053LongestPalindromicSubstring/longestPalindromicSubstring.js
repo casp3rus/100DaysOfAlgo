2b87bfd1103a08c09f937a4e32c91e9f60e863f2
Adrian Popan <adrian.popan@gmail.com>
2021-02-01 20:24:26 -0600

    Day 053 - Longest Palindromic Substring

---
+++ 053LongestPalindromicSubstring/longestPalindromicSubstring.js
@@ -0,0 +1,52 @@
+// Solution 1
+// O(n^3) time / O(n) space
+
+const longestPalindromicSubstring = (string) => {
+  let longest = '';
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i; j < string.length; j++) {
+      const substring = string.slice(i, j + 1);
+      if (substring.length > longest.length && isPalindrome(substring)) {
+        longest = substring;
+      }
+    }
+  }
+  return longest;
+};
+
+const isPalindrome = (string) => {
+  let leftIdx = 0;
+  let rightIdx = string.length - 1;
+  while (leftIdx < rightIdx) {
+    if (string[leftIdx] !== string[rightIdx]) return false;
+    leftIdx++;
+    rightIdx--;
+  }
+  return true;
+};
+
+// Solution 2
+// O(n^2) time / O(n) space
+
+const longestPalindromicSubstring = (string) => {
+  let currentLongest = [0, 1];
+  for (let i = 0; i < string.length; i++) {
+    const odd = getLongestPalindromeFrom(string, i - 1, i + 1);
+    const even = getLongestPalindromeFrom(string, i - 1, i);
+    const longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
+    currentLongest =
+      currentLongest[1] - currentLongest[0] > longest[1] - longest[0]
+        ? currentLongest
+        : longest;
+  }
+  return string.slice(currentLongest[0], currentLongest[1]);
+};
+
+const getLongestPalindromeFrom = (string, leftIdx, rightIdx) => {
+  while (leftIdx >= 0 && rightIdx < string.length) {
+    if (string[leftIdx] !== string[rightIdx]) break;
+    leftIdx--;
+    rightIdx++;
+  }
+  return [leftIdx + 1, rightIdx];
+};

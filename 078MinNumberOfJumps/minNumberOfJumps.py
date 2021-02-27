d286b7091f6c201d14f7b3a26f59ef088c979cc8
Adrian Popan <adrian.popan@gmail.com>
2021-02-26 18:41:36 -0600

    Day 078 - Min Number Of Jumps

---
+++ 078MinNumberOfJumps/minNumberOfJumps.py
@@ -0,0 +1,32 @@
+# Solution 1
+# O(n^2) time / O(n) Space
+
+def minNumberOfJumps(array):
+    jumps = [float("inf") for x in array]
+    jumps[0] = 0
+
+    for i in range(1, len(array)):
+        for j in range(0, i):
+            if array[j] >= i - j:
+                jumps[i] = min(jumps[j] + 1, jumps[i])
+    return jumps[-1]
+
+
+# Solution 2
+# O(n) time / O(1) Space
+
+def minNumberOfJumps(array):
+    if len(array) == 1:
+        return 0
+
+    jumps = 0
+    maxReach = array[0]
+    steps = array[0]
+
+    for i in range(1, len(array) - 1):
+        maxReach = max(maxReach, i + array[i])
+        steps -= 1
+        if steps == 0:
+            jumps += 1
+            steps = maxReach - i
+    return jumps + 1

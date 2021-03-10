2724eccbd944b38222b4fe80c52f0fa798a0ff6f
Adrian Popan <adrian.popan@gmail.com>
2021-03-10 12:04:01 -0600

    Day 089 - Height Balanced Binary Tree

---
+++ 089HeightBalancedBinaryTree/heightBalancedBinaryTree.py
@@ -0,0 +1,38 @@
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+# Solution
+# O(n) time / O(h) space
+# n - number of nodes in the binary tree
+# h - height of the binary tree
+
+class TreeInfo:
+    def __init__(self, isBalanced, height):
+        self.isBalanced = isBalanced
+        self.height = height
+
+
+def heightBalancedBinaryTree(tree):
+    treeInfo = getTreeInfo(tree)
+    return treeInfo.isBalanced
+
+
+def getTreeInfo(node):
+    if node is None:
+        return TreeInfo(True, -1)
+
+    leftSubtreeInfo = getTreeInfo(node.left)
+    rightSubtreeInfo = getTreeInfo(node.right)
+
+    isBalanced = (
+        leftSubtreeInfo.isBalanced
+        and rightSubtreeInfo.isBalanced
+        and abs(leftSubtreeInfo.height - rightSubtreeInfo.height) <= 1
+    )
+    height = max(leftSubtreeInfo.height, rightSubtreeInfo.height) + 1
+    return TreeInfo(isBalanced, height)

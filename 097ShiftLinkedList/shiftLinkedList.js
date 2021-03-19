2116ca9852936d02d0841a9a50548f5861c6549b
Adrian Popan <adrian.popan@gmail.com>
2021-03-18 21:19:07 -0500

    Day 097 - Shift Linked List

---
+++ 097ShiftLinkedList/shiftLinkedList.js
@@ -0,0 +1,36 @@
+// This is the class of the input linked list.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// Solution
+// O(n) time / O(1) space
+
+const shiftLinkedList = (head, k) => {
+  let listLength = 1;
+  let listTail = head;
+
+  while (listTail.next !== null) {
+    listTail = listTail.next;
+    listLength++;
+  }
+
+  const offset = Math.abs(k) % listLength;
+  if (offset === 0) return head;
+
+  const newTailPosition = k > 0 ? listLength - offset : offset;
+  let newTail = head;
+
+  for (let i = 1; i < newTailPosition; i++) {
+    newTail = newTail.next;
+  }
+
+  const newHead = newTail.next;
+  newTail.next = null;
+  listTail.next = head;
+
+  return newHead;
+};

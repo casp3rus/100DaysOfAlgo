e97ac83cd01c3d5e9029c168aaa359baf650b04a
Adrian M Popan <adrian.popan@gmail.com>
2021-06-22 22:36:27 -0500

    Day 120 - challenge description

--- 120MinimumCharactersForWords/minimumCharactersForWords.js
+++ 120MinimumCharactersForWords/minimumCharactersForWords.js
@@ -1,5 +1,8 @@
 // Solution
-// O() time / O() space
+// O(n*l) time / O(c) space
+// n - number of words
+// l - length of the longest word
+// c - number of unique characters across all words
 
 export function minimumCharactersForWords(words) {
   // Write your code here.

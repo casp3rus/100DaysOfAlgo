284b2203fb0222ff5b6dea1de3292f79cb424819
Adrian M Popan <adrian.popan@gmail.com>
2021-06-22 22:20:48 -0500

    Day 120 - Minimum Characters For Words

--- 120MinimumCharactersForWords/minimumCharactersForWords.py
+++ 120MinimumCharactersForWords/minimumCharactersForWords.py
@@ -1,6 +1,45 @@
 # Solution
-# O() time / O() space
+# O(n*l) time / O(c) space
+# n - number of words
+# l - length of the longest word
+# c - number of unique characters across all words
 
 def minimumCharactersForWords(words):
-    # Write your code here.
-    return []
+    maximumCharacterFrequencies = {}
+
+    for word in words:
+      characterFrequencies = countCharacterFrequencies(word)
+      updateMaximumFrequencies(characterFrequencies, maximumCharacterFrequencies)
+
+    return makeArrayFromCharacterFrequencies(maximumCharacterFrequencies)
+
+def countCharacterFrequencies(string):
+  characterFrequencies = {}
+
+  for character in string:
+    if character not in characterFrequencies:
+      characterFrequencies[character] = 0
+
+    characterFrequencies[character] += 1
+  
+  return characterFrequencies
+
+def updateMaximumFrequencies(frequencies, maximumFrequencies):
+  for character in frequencies:
+    frequency = frequencies[character]
+
+    if character in maximumFrequencies:
+      maximumFrequencies[character] = max(frequency, maximumFrequencies[character])
+    else:
+      maximumFrequencies[character] = frequency
+
+def makeArrayFromCharacterFrequencies(characterFrequencies):
+  characters = []
+
+  for character in characterFrequencies:
+    frequency = characterFrequencies[character]
+
+    for _ in range(frequency):
+      characters.append(character)
+
+  return characters

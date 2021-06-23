b9f6c4d6d55b236550632cf9a82afb62246ac0e3
Adrian M Popan <adrian.popan@gmail.com>
2021-06-23 17:13:26 -0500

    Day 120 - Minimum Characters For Words

--- 120MinimumCharactersForWords/minimumCharactersForWords.ts
+++ 120MinimumCharactersForWords/minimumCharactersForWords.ts
@@ -4,7 +4,57 @@
 // l - length of the longest word
 // c - number of unique characters across all words
 
+interface CharacterFrequencies {
+  [character: string]: number;
+}
+
 export function minimumCharactersForWords(words: string[]) {
-  // Write your code here.
-  return [];
+  const maximumCharacterFrequencies: CharacterFrequencies = {};
+
+  for (const word of words) {
+    const characterFrequencies = countCharacterFrequencies(word);
+    updateMaximumFrequencies(characterFrequencies, maximumCharacterFrequencies);
+  }
+
+  return makeArrayFromCharacterFrequencies(maximumCharacterFrequencies);
+}
+
+function countCharacterFrequencies(string: string) {
+  const characterFrequencies: CharacterFrequencies = {};
+
+  for (const character of string) {
+    if (!(character in characterFrequencies)) {
+      characterFrequencies[character] = 0;
+    }
+
+    characterFrequencies[character] ++;
+  }
+
+  return characterFrequencies;
+}
+
+function updateMaximumFrequencies(frequencies: CharacterFrequencies, maximumFrequencies: CharacterFrequencies) {
+  for (const character in frequencies) {
+    const frequency = frequencies[character];
+
+    if (character in maximumFrequencies) {
+      maximumFrequencies[character] = Math.max(frequency, maximumFrequencies[character]);
+    } else {
+      maximumFrequencies[character] = frequency;
+    }
+  }
+}
+
+function makeArrayFromCharacterFrequencies(characterFrequencies: CharacterFrequencies) {
+  const characters: string[] = [];
+
+  for (const character in characterFrequencies) {
+    const frequency = characterFrequencies[character];
+
+    for (let idx = 0; idx < frequency; idx++) {
+      characters.push(character);
+    }
+  }
+
+  return characters
 }
